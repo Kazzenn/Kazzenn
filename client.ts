3187b324ecf617f6a65d6659ec5bf533aea3a581
Kazzenn <73125268+Kazzenn@users.noreply.github.com>
2023-02-20 00:30:55 +0700

    Delete client.ts

--- client.ts
+++
@@ -1,8 +0,0 @@
-  constructor(options?: ClientOptions) {
-    super(options);
-
-    this._silent = options?.silent !== undefined || false;
-    this._silent = !!options?.silent;
-    this._loadClasses = options?.classes || [];
-
-    Client._variablesChar = options?.variablesChar || ":";
